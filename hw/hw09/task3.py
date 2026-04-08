b08158dcbbe549517941e8ebebf7ac744e4abbce
Churei Ivan <ichurey13@gmail.com>
2026-04-08 22:30:55 +0300

    edded task3

---
+++ hw/hw09/task3.py
@@ -0,0 +1,51 @@
+import tkinter as tk
+from pyowm import OWM
+
+API_KEY = 'ef2206ff5da67de63306d0b143e20872'
+owm = OWM(API_KEY)
+mgr = owm.weather_manager()
+
+def get_weather():
+    city = entry_field.get()
+    try:
+        observation = mgr.weather_at_place(city)
+        w = observation.weather
+        
+        temp = w.temperature('celsius')['temp']
+        status = w.detailed_status
+        humidity = w.humidity
+        
+        result = f"Місто: {city}\nСтатус: {status}\nТемп: {temp}°C\nВологість: {humidity}%"
+        label['text'] = result
+    except Exception as e:
+        label['text'] = "Помилка! Місто не знайдено."
+
+HEIGHT = 400
+WIDTH = 500
+
+root = tk.Tk()
+root.title("Weather Application")
+
+canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
+canvas.pack()
+
+frame = tk.Frame(root, bg="deep sky blue", bd=5)
+frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
+
+entry_field = tk.Entry(frame, font=('Courier', 12))
+entry_field.place(relx=0, rely=0, relwidth=0.65, relheight=1)
+
+button = tk.Button(frame, 
+                   text="Отримати погоду", 
+                   bg="gray", fg="white", 
+                   font=('Courier', 8), 
+                   command=get_weather)
+button.place(relx=0.7, rely=0, relwidth=0.3, relheight=1)
+
+lower_frame = tk.Frame(root, bg='gold', bd=10)
+lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
+
+label = tk.Label(lower_frame, font=('Courier', 12), justify='left', anchor='nw')
+label.place(relx=0, rely=0, relwidth=1, relheight=1)
+
+root.mainloop()
